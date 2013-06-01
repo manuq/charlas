071511a232fe296f5c4b9d11c191bac21da3aac8
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-06-01 14:22:20 -0300

    parece una charla

---
+++ charla-pyday-rosario/build.sh
@@ -0,0 +1,1 @@
+rst2pdf -s slides.style -b1 -e inkscape charla.txt --fit-background-mode=scale
--- charla-pyday-rosario/masticar.py
+++ charla-pyday-rosario/masticar.py
@@ -23,7 +23,7 @@ with open('respuestas.csv', 'r') as fd:
         x[0],
         int(x[1]) if x[1] else 0,
         x[2]=='Si',
-        x[3]=='Si',
+        x[3]=='Si' and x[2]=='Si',  # Corregir para lideres que no hacen FLOSS porque FOO
         x[4]=='Si',
         x[5]=='Si',
         int(x[6]) if x[6] else 0,
@@ -45,24 +45,24 @@ print "Gente con trabajo:", len([x for x in resultados if x.ocupado])
 print "Gente sin trabajo:", len([x for x in resultados if not x.ocupado])
 
 c_ocup_no_floss = len([x for x in resultados if x.ocupado and not x.floss])
-print "Porcentaje de ocupacion en no FLOSS: %f.2" % ((100.*c_ocup_no_floss)/c_no_floss)
+print "Porcentaje de ocupacion en no FLOSS: %.2f%%" % ((100.*c_ocup_no_floss)/c_no_floss)
 
 c_ocup_floss = len([x for x in resultados if x.ocupado and x.floss])
-print "Porcentaje de ocupacion en FLOSS: %f.2" % ((100.*c_ocup_floss)/c_floss)
+print "Porcentaje de ocupacion en FLOSS: %.2f%%" % ((100.*c_ocup_floss)/c_floss)
 
 c_ocup_lider = len([x for x in resultados if x.ocupado and x.floss_leader])
-print "Porcentaje de ocupacion en lideres FLOSS: %f.2" % ((100.*c_ocup_lider)/c_lider)
+print "Porcentaje de ocupacion en lideres FLOSS: %.2f%%" % ((100.*c_ocup_lider)/c_lider)
 
 print "--------------"
 
-x1 = [x.edad_laboral for x in resultados if not x.floss]
-x2 = [x.edad_laboral for x in resultados if x.floss]
-x3 = [x.edad_laboral for x in resultados if x.floss_leader]
-plt.hist([x1, x2, x3], normed=1, range=(0,25))
+#x1 = [x.edad_laboral for x in resultados if not x.floss]
+#x2 = [x.edad_laboral for x in resultados if x.floss]
+#x3 = [x.edad_laboral for x in resultados if x.floss_leader]
+#plt.hist([x1, x2, x3], normed=1, range=(0,25))
 #plt.hist([x.edad_floss for x in resultados if x.floss], normed=1, facecolor='b', alpha=.5)
 #plt.hist(, normed=1, facecolor='r', alpha=.2, range=(0,25))
-plt.xlabel('Edad Laboral')
-plt.title('Hace cuanto trabaja la gente? [no-floss/floss/lideres]')
+#plt.xlabel('Edad Laboral')
+#plt.title('Hace cuanto trabaja la gente? [no-floss/floss/lideres]')
 #plt.show()
 
 print "Cuando contratas te sirve si el candidato hace FLOSS?"
@@ -72,7 +72,8 @@ print "NO:", len([x for x in resultados if not x.contrata_por_floss])
 print "EPA"
 
 print "Claro, no todo el mundo contrata :-)"
-
+print "Cuantos contratan?"
+print len([x for x in resultados if x.contrata])
 print "Cuando contratas (si lo haces) te sirve si el candidato hace FLOSS?"
 c1 = len([x for x in resultados if x.contrata and x.contrata_por_floss])
 c2 = len([x for x in resultados if x.contrata and not x.contrata_por_floss])
@@ -84,11 +85,6 @@ print "EPA!"
 print "Cuando contratas te sirve si el candidato hace FLOSS aunque hayas dicho que no contratas?"
 print "SI:", len([x for x in resultados if not x.contrata and x.contrata_por_floss])
 
-print "Eso no tiene sentido, pibe."
-print "Asi es la estadistica."
-
-
-print "Esa estadistica anterior es medio mentira"
 print "Si contratas y haces FLOSS, te sirve que el candidato haga FLOSS?"
 c1 = len([x for x in resultados if x.contrata and x.contrata_por_floss and x.floss])
 c2 = len([x for x in resultados if x.contrata and not x.contrata_por_floss and x.floss])
